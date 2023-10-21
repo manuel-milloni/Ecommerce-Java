54ae7bfaab69e8b87421cee063ccd22e6fc6a51c
Manuel Milloni <milloni3710@gmail.com>
2023-10-21 11:49:27 -0300

    Agregado traer Stock actual del producto al modificar

--- src/main/java/controlador/Controlador.java
+++ src/main/java/controlador/Controlador.java
@@ -1,28 +1,16 @@
 
 package controlador;
 
-
-
-
-
-
 import modelo.Producto;
 
 import modelo.ProductoDAO;
-
-
-
-
 
 import java.util.ArrayList;
 
 import java.util.List;
 
-
-
 import modelo.TipoProducto;
 import modelo.TipoProductoDAO;
-
 
 import java.io.File;
 import java.io.IOException;
@@ -34,168 +22,154 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 
-
-
 @MultipartConfig
 @WebServlet("/Controlador")
 public class Controlador extends HttpServlet {
-	
+
 	private static final long serialVersionUID = 1L;
-    private ProductoDAO pDAO = new ProductoDAO();
-    private Producto producto = new Producto();
-    private TipoProductoDAO tpDAO = new TipoProductoDAO();
-    private List<TipoProducto> tiposProducto = new ArrayList<>();
-    private TipoProducto tp = new TipoProducto();
-    private Producto p = new Producto();
-    private List<Producto> productos = new ArrayList<>();
-    private static final String UPLOAD_DIR = "images";
+	private ProductoDAO pDAO = new ProductoDAO();
+	private Producto producto = new Producto();
+	private TipoProductoDAO tpDAO = new TipoProductoDAO();
+	private List<TipoProducto> tiposProducto = new ArrayList<>();
+	private TipoProducto tp = new TipoProducto();
+	private Producto p = new Producto();
+	private List<Producto> productos = new ArrayList<>();
+	private static final String UPLOAD_DIR = "images";
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 
-    }
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String menu = request.getParameter("menu");
-        String accion = request.getParameter("accion");
-        if (menu.equals("Producto")) {
-            switch (accion) {
-                
-             case "Producto": {
-                    if(request.getSession().getAttribute("authEmpleado")!=null){
-                        tiposProducto = tpDAO.getAll();
-                    productos = pDAO.getAll();
-                    request.setAttribute("tiposProducto", tiposProducto);
-                    request.setAttribute("productos", productos);
-                    request.getRequestDispatcher("admin/producto.jsp").forward(request, response);
-                    break;
-                    
-                    } else {
-                      request.getRequestDispatcher("Admin").forward(request, response);
-                    
-                    }
-                    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String menu = request.getParameter("menu");
+		String accion = request.getParameter("accion");
+		if (menu.equals("Producto")) {
+			switch (accion) {
 
-                }
-                
-                case "Editar":{
-                  int id=Integer.parseInt(request.getParameter("id"));
-                  Producto producto=null;
-                  producto=pDAO.getById(id);
-                  if(producto!=null){
-                      tiposProducto = tpDAO.getAll();
-                       request.setAttribute("tiposProducto", tiposProducto);
-                      request.setAttribute("producto", producto);
-                      request.getRequestDispatcher("admin/productoModificar.jsp").forward(request, response);
-                  
-                  } else  {
-                     response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                  
-                  }
-                  break;
-                  
-                
-                }
-            }
-        }
-    }
+			case "Producto": {
+				if (request.getSession().getAttribute("authEmpleado") != null) {
+					tiposProducto = tpDAO.getAll();
+					productos = pDAO.getAll();
+					request.setAttribute("tiposProducto", tiposProducto);
+					request.setAttribute("productos", productos);
+					request.getRequestDispatcher("admin/producto.jsp").forward(request, response);
+					break;
 
+				} else {
+					request.getRequestDispatcher("Admin").forward(request, response);
 
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-       
-        String menu = request.getParameter("menu");
-        String accion = request.getParameter("accion");
-        if (menu.equals("Producto")) {
-            switch (accion) {
+				}
 
-                case "NuevoProducto": {
-                    p.setDescripcion(request.getParameter("descripcion"));
-                    p.setPrecio(Double.parseDouble(request.getParameter("precio")));
-                    p.setIdTipo(Integer.parseInt(request.getParameter("tipoProducto")));
-                    p.setStock(Integer.parseInt(request.getParameter("stock")));
+			}
 
-                    //Imagen ----------------------------------------------------------------------
-                       //Obtener ruta absoluta de la App  
-                      String applicationPath = request.getServletContext().getRealPath("");
-                      System.out.println("Aplication path: "+applicationPath);
-                      
-                      //File path
-                      String uploadFilePath = applicationPath+UPLOAD_DIR+File.separator;
-                      System.out.println("File path: "+uploadFilePath);
-                     
-                     
-                      
-                      
-                      //Folder images
-                      File fileSaveDir = new File(uploadFilePath);
-                     
-                      if(!fileSaveDir.exists()) {
-                    	    //Si el direcorio no existe, lo creo
-                    	    fileSaveDir.mkdirs();
-                    	 
-                      }
-                      
-                      
-                      
-                      String fileName=null;
-                      
-                      for(Part part : request.getParts()) {
-                    	   fileName= getFileName(part);
-                    	   System.out.println("Filename: "+fileName);
-                    	   System.out.println("Write: "+uploadFilePath+fileName);
-                    	   if(fileName!=null && !fileName.equalsIgnoreCase("")) {
-                    		   part.write(uploadFilePath+fileName);   
-                    		   
-                    	   }
-                    	  
-                    	   
-                    	  
-                      }
-                      
-                      p.setImagen(UPLOAD_DIR+File.separator+fileName);
-                      // -- --------------------------------------------------------------------------------------------------  
-                    
-          
+			case "Editar": {
+				int id = Integer.parseInt(request.getParameter("id"));
+				Producto producto = null;
+				producto = pDAO.getById(id);
+				if (producto != null) {
+					tiposProducto = tpDAO.getAll();
+					request.setAttribute("tiposProducto", tiposProducto);
+					request.setAttribute("producto", producto);
+					request.getRequestDispatcher("admin/productoModificar.jsp").forward(request, response);
 
-                    pDAO.save(p);
-                    
-            
-                    response.sendRedirect("Controlador?menu=Producto&accion=Producto");
-                    break;
+				} else {
+					response.setStatus(HttpServletResponse.SC_NOT_FOUND);
 
-                }
-                
-                case "modificar":{
-                    p.setIdProducto(Integer.parseInt(request.getParameter("id")));
-                    p.setDescripcion(request.getParameter("descripcion"));
-                    p.setPrecio(Double.parseDouble(request.getParameter("precio")));
-                    p.setIdTipo(Integer.parseInt(request.getParameter("tipoProducto")));
-                    p.setStock(Integer.parseInt(request.getParameter("stock")));
-                    pDAO.update(p);
-                    response.sendRedirect("Controlador?menu=Producto&accion=Producto");
-                
-                }
-            }
-        }
-    }
-    
-    private String getFileName(Part part) {
-        String contentDisp = part.getHeader("content-disposition");
-        System.out.println("content-disposition header= "+contentDisp);
-        String[] tokens = contentDisp.split(";");
-        for (String token : tokens) {
-            if (token.trim().startsWith("filename")) {
-                return token.substring(token.indexOf("=") + 2, token.length()-1);
-            }
-        }
-        return "";
-    }
+				}
+				break;
 
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+			}
+			}
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		String menu = request.getParameter("menu");
+		String accion = request.getParameter("accion");
+		if (menu.equals("Producto")) {
+			switch (accion) {
+
+			case "NuevoProducto": {
+				p.setDescripcion(request.getParameter("descripcion"));
+				p.setPrecio(Double.parseDouble(request.getParameter("precio")));
+				p.setIdTipo(Integer.parseInt(request.getParameter("tipoProducto")));
+				p.setStock(Integer.parseInt(request.getParameter("stock")));
+
+				// Imagen ----------------------------------------------------------------------
+				// Obtener ruta absoluta de la App
+				String applicationPath = request.getServletContext().getRealPath("");
+				System.out.println("Aplication path: " + applicationPath);
+
+				// File path
+				String uploadFilePath = applicationPath + UPLOAD_DIR + File.separator;
+				System.out.println("File path: " + uploadFilePath);
+
+				// Folder images
+				File fileSaveDir = new File(uploadFilePath);
+
+				if (!fileSaveDir.exists()) {
+					// Si el direcorio no existe, lo creo
+					fileSaveDir.mkdirs();
+
+				}
+
+				String fileName = null;
+
+				for (Part part : request.getParts()) {
+					fileName = getFileName(part);
+					System.out.println("Filename: " + fileName);
+					System.out.println("Write: " + uploadFilePath + fileName);
+					if (fileName != null && !fileName.equalsIgnoreCase("")) {
+						part.write(uploadFilePath + fileName);
+
+					}
+
+				}
+
+				p.setImagen(UPLOAD_DIR + File.separator + fileName);
+				// --
+				// --------------------------------------------------------------------------------------------------
+
+				pDAO.save(p);
+
+				response.sendRedirect("Controlador?menu=Producto&accion=Producto");
+				break;
+
+			}
+
+			case "modificar": {
+				p.setIdProducto(Integer.parseInt(request.getParameter("id")));
+				p.setDescripcion(request.getParameter("descripcion"));
+				p.setPrecio(Double.parseDouble(request.getParameter("precio")));
+				p.setIdTipo(Integer.parseInt(request.getParameter("tipoProducto")));
+				p.setStock(Integer.parseInt(request.getParameter("stock")));
+				pDAO.update(p);
+				response.sendRedirect("Controlador?menu=Producto&accion=Producto");
+
+			}
+			}
+		}
+	}
+
+	private String getFileName(Part part) {
+		String contentDisp = part.getHeader("content-disposition");
+		System.out.println("content-disposition header= " + contentDisp);
+		String[] tokens = contentDisp.split(";");
+		for (String token : tokens) {
+			if (token.trim().startsWith("filename")) {
+				return token.substring(token.indexOf("=") + 2, token.length() - 1);
+			}
+		}
+		return "";
+	}
+
+	@Override
+	public String getServletInfo() {
+		return "Short description";
+	}// </editor-fold>
 
 }
--- src/main/webapp/admin/productoModificar.jsp
+++ src/main/webapp/admin/productoModificar.jsp
@@ -3,168 +3,195 @@
 <%@page import="java.util.List"%>
 <%@page import="modelo.TipoProductoDAO"%>
 <%@page import="modelo.ProductoDAO"%>
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Producto</title>
-  
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<title>Producto</title>
+
+<link
+	href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css"
+	rel="stylesheet"
+	integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
+	crossorigin="anonymous">
 </head>
 <body>
-    <%@include file="../estructura/navAdmin.jsp" %>
-    <div class="d-flex">
-        <br>
-        <div class="card col-sm-4">
-            
-               <div class="card-body">
-        <h2>Producto</h2>
-        
-         <% if (request.getSession().getAttribute("mensaje") != null) { %>
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        <strong><%= request.getSession().getAttribute("mensaje") %></strong>
-         <% request.getSession().setAttribute("mensaje", null); %>
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-        <% } %>
-        
-        <form action="Controlador?menu=Producto&accion=modificar&id=${producto.getIdProducto()}" method="post" enctype="multipart/form-data">
-            
-            <div class="form-group row">
-                <label for="descripcion" class="col-sm-3 col-form-label">Descripción:</label>
-            <div class="col-sm-9">
-            <input type="text" id="descripcion" name="descripcion" class="form-control" value="${producto.getDescripcion()}" maxlength="30" required>
-            </div>
-            </div>
-            
-         <div class="form-group row">
-            <label for="precio" class="col-sm-3 col-form-label">Precio:</label>
-        <div class="col-sm-9">
-        <input type="number" id="precio" name="precio" class="form-control" value="${producto.getPrecio()}" required step="any">
-        </div>
-         </div>
+	<%@include file="../estructura/navAdmin.jsp"%>
+	<div class="d-flex">
+		<br>
+		<div class="card col-sm-4">
 
-            
-            <div class="form-group row">
-    <label for="tipoProducto" class="col-sm-3 col-form-label">Tipo:</label>
-    
-    <div class="col-sm-9">
-        <select id="tipoProducto" name="tipoProducto" class="form-control" required>
-            <c:forEach var="tp" items="${tiposProducto}">
-                <option value="${tp.getIdTipo()}" ${tp.getDescripcion() eq opcion ? 'selected' : ''}>${tp.getDescripcion()}</option>
-            </c:forEach>
-        </select>
-    </div>
-            </div>
-    
-  <div class="form-group row">
-    <label for="stock" class="col-sm-3 col-form-label">Stock:</label>
-    <div class="col-sm-9">
-        <input type="number" id="stock" name="stock" class="form-control" value="" required step="1">
-    </div>
-</div>
-    <div class="form-group row">
-     <label for="imagen" class="col-sm-3 col-form-label">Imagen:</label>
-      <div class="col-sm-9">
-        <input type="file" name="imagen" id="imagen" accept="image/*" >
-      </div>
-    </div>
-    
+			<div class="card-body">
+				<h2>Producto</h2>
+
+				<%
+				if (request.getSession().getAttribute("mensaje") != null) {
+				%>
+				<div class="alert alert-success alert-dismissible fade show"
+					role="alert">
+					<strong><%=request.getSession().getAttribute("mensaje")%></strong>
+					<%
+					request.getSession().setAttribute("mensaje", null);
+					%>
+					<button type="button" class="btn-close" data-bs-dismiss="alert"
+						aria-label="Close"></button>
+				</div>
+				<%
+				}
+				%>
+
+				<form
+					action="Controlador?menu=Producto&accion=modificar&id=${producto.getIdProducto()}"
+					method="post" enctype="multipart/form-data">
+
+					<div class="form-group row">
+						<label for="descripcion" class="col-sm-3 col-form-label">Descripción:</label>
+						<div class="col-sm-9">
+							<input type="text" id="descripcion" name="descripcion"
+								class="form-control" value="${producto.getDescripcion()}"
+								maxlength="90" required>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="precio" class="col-sm-3 col-form-label">Precio:</label>
+						<div class="col-sm-9">
+							<input type="number" id="precio" name="precio"
+								class="form-control" value="${producto.getPrecio()}" required
+								step="any">
+						</div>
+					</div>
 
 
+					<div class="form-group row">
+						<label for="tipoProducto" class="col-sm-3 col-form-label">Tipo:</label>
 
-            <br>
-            <div class="text-center">
-                <a class="btn btn-warning" href="Controlador?menu=Producto&accion=Producto">Cancelar</a>
-                <input class="btn btn-success" type="submit" value="Modificar">
-           
-                
-                </div>
-            
-        </form>
-    </div>
-        
-            
-        </div>
-        
-        <div class="col-sm-8">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>DESCRIPCION</th>
-                        <th>PRECIO</th>
-                        <th>TIPO</th>
-                        <th>STOCK</th>
-                        <th>IMAGEN</th>
-                        <th>ACCIONES</th>
-                        
-                        
-                    </tr>
-                    
-                    
-                </thead>
-                
-                <%
-                   ProductoDAO pDAO=new ProductoDAO();
-                   TipoProductoDAO tpDAO=new TipoProductoDAO();
-                   List<Producto> productos=pDAO.getAll();
-                   for(Producto p: productos){
-                              
-                    
-                           TipoProducto tp=tpDAO.getById(p.getIdTipo());
-                         %>
-                        <tbody>
-                        <tr>
-                            
-                            <td><%=p.getIdProducto()%></td>
-                            <td><%=p.getDescripcion()%></td>
-                            <td><%=p.getPrecio()%></td>
-                            <td><%=tp.getDescripcion()%></td>
-                            <td><%=p.getStock()%></td>
-                            <td>
-                                 
-                                 <img src="../img/calendario.PNG" alt="Imagen del producto" class="card-img-top">
-                            </td>
-                            <td>
-                            <a class="btn btn-warning" href="Controlador?menu=Producto&accion=Editar&id=<%= p.getIdProducto()%>">Editar</a>
-                            <a class="btn btn-danger" href="Controlador?menu=Producto&accion=Eliminar&id=<%= p.getIdProducto()%>">Eliminar</a>
-                        </td>
-                            
-                            
-                        </tr>
-                        
-                        
-                    </tbody>
-                    
-                    
-                    <%}
-                
-                %>
-                
-                
-                
-                
-               
-                
-                
-                
-            </table>
-            
-            
-            
-        </div>
-        
-        
-        
-        
-     
-        
-    </div>
-    
-        
-         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+						<div class="col-sm-9">
+							<select id="tipoProducto" name="tipoProducto"
+								class="form-control" required>
+								<c:forEach var="tp" items="${tiposProducto}">
+									<option value="${tp.getIdTipo()}"
+										${tp.getDescripcion() eq opcion ? 'selected' : ''}>${tp.getDescripcion()}</option>
+								</c:forEach>
+							</select>
+						</div>
+					</div>
+
+					<div class="form-group row">
+						<label for="stock" class="col-sm-3 col-form-label">Stock:</label>
+						<div class="col-sm-9">
+							<input type="number" id="stock" name="stock" class="form-control"
+								value="${producto.getStock()}" required step="1">
+						</div>
+					</div>
+					<div class="form-group row">
+						<label for="imagen" class="col-sm-3 col-form-label">Imagen:</label>
+						<div class="col-sm-9">
+							<input type="file" name="imagen" id="imagen" accept="image/*">
+						</div>
+					</div>
+
+
+
+
+					<br>
+					<div class="text-center">
+						<a class="btn btn-warning"
+							href="Controlador?menu=Producto&accion=Producto">Cancelar</a> <input
+							class="btn btn-success" type="submit" value="Modificar">
+
+
+					</div>
+
+				</form>
+			</div>
+
+
+		</div>
+
+		<div class="col-sm-8">
+			<table class="table table-hover">
+				<thead>
+					<tr>
+						<th>ID</th>
+						<th>DESCRIPCION</th>
+						<th>PRECIO</th>
+						<th>TIPO</th>
+						<th>STOCK</th>
+						<th>IMAGEN</th>
+						<th>ACCIONES</th>
+
+
+					</tr>
+
+
+				</thead>
+
+				<%
+				ProductoDAO pDAO = new ProductoDAO();
+				TipoProductoDAO tpDAO = new TipoProductoDAO();
+				List<Producto> productos = pDAO.getAll();
+				for (Producto p : productos) {
+
+					TipoProducto tp = tpDAO.getById(p.getIdTipo());
+				%>
+				<tbody>
+					<tr>
+
+						<td><%=p.getIdProducto()%></td>
+						<td><%=p.getDescripcion()%></td>
+						<td><%=p.getPrecio()%></td>
+						<td><%=tp.getDescripcion()%></td>
+						<td><%=p.getStock()%></td>
+						<td><img src="../img/calendario.PNG"
+							alt="Imagen del producto" class="card-img-top"></td>
+						<td><a class="btn btn-warning"
+							href="Controlador?menu=Producto&accion=Editar&id=<%=p.getIdProducto()%>">Editar</a>
+							<a class="btn btn-danger"
+							href="Controlador?menu=Producto&accion=Eliminar&id=<%=p.getIdProducto()%>">Eliminar</a>
+						</td>
+
+
+					</tr>
+
+
+				</tbody>
+
+
+				<%
+				}
+				%>
+
+
+
+
+
+
+
+
+			</table>
+
+
+
+		</div>
+
+
+
+
+
+
+	</div>
+
+
+	<script
+		src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
+		integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
+		crossorigin="anonymous"></script>
+	<script
+		src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
+		integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
+		crossorigin="anonymous"></script>
 </body>
 </html>
